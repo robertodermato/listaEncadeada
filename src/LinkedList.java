3a1530429caab2ffe8b3e1ca5a2e11c5a3cae1dc
Roberto <robertodermato@gmail.com>
2019-09-20 14:55:22 -0300

    Classe Nodo

---
+++ src/LinkedList.java
@@ -0,0 +1,15 @@
+public class LinkedList {
+
+    //Classe Node
+    private class Node {
+
+        public Integer element;
+        public Node next;
+
+        public Node(Integer e) {
+            element = e;
+            next = null;
+        }
+    }
+
+}
