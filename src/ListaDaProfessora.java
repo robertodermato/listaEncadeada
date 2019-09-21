bd77c9f389818c144ee4fef2b4e59c1a3b58de58
Roberto <robertodermato@gmail.com>
2019-09-20 23:08:27 -0300

    lista da professora

---
+++ src/ListaDaProfessora.java
@@ -0,0 +1,237 @@
+public class ListaDaProfessora {
+
+
+    // Classe interna Node
+    private class Node {
+
+        public Integer element;
+        public Node next;
+
+        public Node(Integer element) {
+            this.element = element;
+            next = null;
+        }
+    }
+
+    // Referencia para o primeiro elemento da lista encadeada.
+    private Node head;
+    // Referencia para o ultimo elemento da lista encadeada.
+    private Node tail;
+    // Contador para a quantidade de elementos que a lista contem.
+    private int count;
+
+    /**
+     * Construtor da lista
+     */
+    public ListaDaProfessora() {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    /**
+     * Adiciona um elemento ao final da lista
+     *
+     * @param element elemento a ser adicionado ao final da lista
+     */
+    public void add(Integer element) {
+        Node aux = new Node(element);
+        if (head == null) {
+            head = aux;
+        } else {
+            tail.next = aux;
+        }
+        tail = aux;
+        count++;
+    }
+
+    /**
+     * Insere um elemento em uma determinada posicao da lista
+     *
+     * @param index   a posicao da lista onde o elemento sera inserido
+     * @param element elemento a ser inserido
+     * @throws IndexOutOfBoundsException se (index < 0 || index > size())
+     */
+    public void add(int index, Integer element) {
+
+        // Desenvolva o algoritmo para este metodo
+
+    }
+
+    /**
+     * Retorna o elemento de uma determinada posicao da lista
+     *
+     * @param index a posição da lista
+     * @return o elemento da posicao especificada
+     * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
+     */
+    public Integer get(int index) {
+        if ((index < 0) || (index >= count)) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == count - 1)
+            return tail.element;
+        Node aux = head;
+        int c = 0;
+        while (c < index) {
+            aux = aux.next;
+            c++;
+        }
+        return aux.element;
+    }
+
+    /**
+     * Substitui o elemento armanzenado em uma determinada posicao da lista pelo
+     * elemento indicado
+     *
+     * @param index   a posicao da lista
+     * @param element o elemento a ser armazenado na lista
+     * @return o elemento armazenado anteriormente na posicao da lista
+     * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
+     */
+    public Integer set(int index, Integer element) {
+        if ((index < 0) || (index >= count)) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node aux = head;
+        for (int i = 0; i < index; i++) {
+            aux = aux.next;
+        }
+        Integer tmp = aux.element;
+        aux.element = element;
+        return tmp;
+
+    }
+
+    /**
+     * Remove a primeira ocorrencia do elemento na lista, se estiver presente
+     *
+     * @param element o elemento a ser removido
+     * @return true se a lista contem o elemento especificado
+     */
+    public boolean remove(Integer element) {
+        if (element == null) {
+            return false;
+        }
+        if (count == 0) {
+            return false;
+        }
+
+        if (head.element.equals(element)) { // remocao do primeiro
+            head = head.next;
+            if (count == 1) { // se havia so um elemento na lista
+                tail = null;
+            }
+            count--;
+            return true;
+        }
+
+        Node ant = head;
+        Node aux = head.next;
+
+        for (int i = 1; i < count; i++) {
+            if (aux.element.equals(element)) {
+                if (aux == tail) { // remocao do ultimo
+                    tail = ant;
+                    tail.next = null;
+                } else { // remocao do meio
+                    ant.next = aux.next;
+                }
+                count--;
+                return true;
+            }
+            ant = ant.next;
+            aux = aux.next;
+        }
+
+        return false;
+    }
+
+    /**
+     * Retorna true se a lista nao contem elementos
+     *
+     * @return true se a lista nao contem elementos
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    /**
+     * Retorna o numero de elementos da lista
+     *
+     * @return o numero de elementos da lista
+     */
+    public int size() {
+        return count;
+    }
+
+    /**
+     * Esvazia a lista
+     */
+    public void clear() {
+        head = null;
+        tail = null;
+        count = 0;
+    }
+
+    /**
+     * Remove o elemento de uma determinada posicao da lista
+     *
+     * @param index a posicao da lista
+     * @return o elemento que foi removido da lista
+     * @throws IndexOutOfBoundsException se (index < 0 || index >= size())
+     */
+    public Integer removeByIndex(int index) {
+
+        // Desenvolva o algoritmo para este metodo
+        return 0;
+
+    }
+
+    /**
+     * Retorna o indice da primeira ocorrencia do elemento na lista, ou -1 se a
+     * lista nao contem o elemento
+     *
+     * @param element o elemento a ser buscado
+     * @return o indice da primeira ocorrencia do elemento na lista, ou -1 se a
+     * lista nao contem o elemento
+     */
+    public int indexOf(Integer element) {
+
+        // Desenvolva o algoritmo para este metodo
+        return 0;
+
+    }
+
+    /**
+     * Retorna true se a lista contem o elemento especificado
+     *
+     * @param element o elemento a ser testado
+     * @return true se a lista contem o elemento especificado
+     */
+    public boolean contains(Integer element) {
+
+        // Desenvolva o algoritmo para este metodo
+        return false;
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+
+        Node aux = head;
+
+        while (aux != null) {
+            s.append(aux.element.toString());
+            s.append("\n");
+            aux = aux.next;
+        }
+
+        return s.toString();
+    }
+
+}
+
+
+
